ba2b6f0db4f93410f569ce59df44459f2009ec2f
SC <github.com@genkisugi.net>
2015-07-29 15:41:53 -0700

    done first pass of getting manager into a separate existence

---
+++ tests/core.test.js
@@ -0,0 +1,36 @@
+////
+//// Some unit testing for package bbop-rest-manager.
+////
+
+var chai = require('chai');
+chai.config.includeStack = true;
+var assert = chai.assert;
+var manager = require('..');
+
+// Correct environment, ready testing.
+var bbop = require('bbop-core');
+var response = require('bbop-rest-response').base;
+var response_json = require('bbop-rest-response').json;
+
+///
+/// Start unit testing.
+///
+
+describe('overall bbop-response-minerva', function(){
+
+    it('some stuff', function(){
+
+	//
+	
+
+    });
+});
+
+describe('try against a real server', function(){
+
+    it('make a connect and look', function(){
+
+	
+
+    });
+});
