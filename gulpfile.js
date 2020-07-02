d000c1c00a72910ccd997da2a265ef22a60c56ae
SC <github.com@genkisugi.net>
2020-07-02 16:22:15 -0700

    remove baseline and ensure npm for gulp push to npm

--- gulpfile.js
+++ gulpfile.js
@@ -59,7 +59,7 @@ gulp.task('doc', ['jsdoc']);
 gulp.task('jsdoc', ['clean'], function(cb) {
     gulp.src('')
         .pipe(shell([
-	    './node_modules/.bin/jsdoc --verbose --template ./node_modules/jsdoc-baseline --readme ./README.md --destination ./doc/ ./lib/*.js'
+	    './node_modules/.bin/jsdoc --verbose --readme ./README.md --destination ./doc/ ./lib/*.js'
 	]));
     cb(null);
 });
@@ -89,7 +89,7 @@ gulp.task('publish-npm', function() {
     var npm = require("npm");
     npm.load(function (er, npm) {
 	// NPM
-	npm.commands.publish();	
+	npm.commands.publish();
     });
 });
 
